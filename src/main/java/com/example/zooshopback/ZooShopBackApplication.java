1e3242cf86aa40a7aabf2c0371c7795eb36bee77
Bulad <tbulad222@gmail.com>
2023-10-23 17:45:20 +0300

    first commit

---
+++ src/main/java/com/example/zooshopback/ZooShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.zooshopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooShopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zooshopback/ZooShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zooshopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
